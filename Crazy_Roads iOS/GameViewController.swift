2d88b13053b866ca7d6202f4f847c745edbc9c8d
Calvin Teater <laughingglint3@gmail.com>
2023-04-18 15:44:02 -0500

    Base 3D Setup

--- Crazy_Roads iOS/GameViewController.swift
+++ Crazy_Roads iOS/GameViewController.swift
@@ -6,54 +6,25 @@
 //
 
 import UIKit
+import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
 
-    var gameView: SCNView {
-        return self.view as! SCNView
-    }
-    
-    var gameController: GameController!
+    var scene: SCNScene!
+    var sceneView: SCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.gameController = GameController(sceneRenderer: gameView)
-        
-        // Allow the user to manipulate the camera
-        self.gameView.allowsCameraControl = true
-        
-        // Show statistics such as fps and timing information
-        self.gameView.showsStatistics = true
-        
-        // Configure the view
-        self.gameView.backgroundColor = UIColor.black
-        
-        // Add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        var gestureRecognizers = gameView.gestureRecognizers ?? []
-        gestureRecognizers.insert(tapGesture, at: 0)
-        self.gameView.gestureRecognizers = gestureRecognizers
+        setupScene()
     }
     
-    @objc
-    func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
-        // Highlight the tapped nodes
-        let p = gestureRecognizer.location(in: gameView)
-        gameController.highlightNodes(atPoint: p)
+    //initializes both scene and sceneView properties
+    func setupScene() {
+        sceneView = (view as! SCNView)
+        scene = SCNScene()
+        
+        sceneView.scene = scene
     }
     
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-
 }

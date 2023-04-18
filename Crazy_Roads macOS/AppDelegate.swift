82ac3401ebc3a7f2d024d3b694371c031f9bee06
Calvin Teater <laughingglint3@gmail.com>
2023-04-18 15:29:19 -0500

    Initial Commit

---
+++ Crazy_Roads macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Crazy_Roads macOS
+//
+//  Created by Calvin Grant Teater on 4/18/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+

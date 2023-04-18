a79f8546614a9eb7fef59b7d65df148233c027ee
Calvin Teater <laughingglint3@gmail.com>
2023-04-18 16:03:20 -0500

    implemented functions to add a floor node and camera node, the camera is currently looking straight down at the floor

--- Crazy_Roads iOS/GameViewController.swift
+++ Crazy_Roads iOS/GameViewController.swift
@@ -10,13 +10,18 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    
+    //global variables
     var scene: SCNScene!
     var sceneView: SCNView!
+    
+    var cameraNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScene()
+        setupFloor()
+        setupCamera()
     }
     
     //initializes both scene and sceneView properties
@@ -27,4 +32,24 @@ class GameViewController: UIViewController {
         sceneView.scene = scene
     }
     
+    //Create floor to use it as a plane, so we can position all of our elements on
+    func setupFloor() {
+        let floor = SCNFloor()
+        floor.firstMaterial?.diffuse.contents = UIImage(named: "Art.scnassets/darkgrass.png")
+        floor.firstMaterial?.diffuse.wrapS = .repeat
+        floor.firstMaterial?.diffuse.wrapT = .repeat
+        floor.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(12.5, 12.5, 12.5)
+        floor.reflectivity = 0.01
+        
+        let floorNode = SCNNode(geometry: floor)
+        scene.rootNode.addChildNode(floorNode)
+    }
+    
+    //creates camera to look down at the grass floor
+    func setupCamera() {
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 0)
+        cameraNode.eulerAngles = SCNVector3(x: -.pi/2, y: 0, z: 0)
+        scene.rootNode.addChildNode(cameraNode)
+    }
 }

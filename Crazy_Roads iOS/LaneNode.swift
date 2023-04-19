7c489462dc14e9d82397dbdddb85d913190ad84e
Calvin Teater <laughingglint3@gmail.com>
2023-04-19 10:59:17 -0500

    created traffic node class with properties holding the type of vehicle driving and the its direction

--- Crazy_Roads iOS/LaneNode.swift
+++ Crazy_Roads iOS/LaneNode.swift
@@ -11,6 +11,24 @@ enum LaneType {
     case grass, road
 }
 
+//new class with properties for determining the type of vehicle driving on the road and the its direction
+class TrafficNode: SCNNode {
+    
+    let type: Int
+    let directionRight: Bool
+    
+    init(type: Int, directionRight: Bool) {
+        self.type = type
+        self.directionRight = directionRight
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class LaneNode: SCNNode {
     
     let type: LaneType
@@ -59,7 +77,19 @@ class LaneNode: SCNNode {
                     laneNode.addChildNode(vegetation)
                 }
             } else if type == .road {
-                
+                carGap += 1
+                if carGap > 3 {
+                    guard let trafficNode = trafficNode else {
+                        continue
+                    }
+                    if randomBool(odds: 4) {
+                        carGap = 0
+                        let vehicle = getVehicle(for: trafficNode.type)
+                        vehicle.position = SCNVector3(x: 10 - Float(index), y: 0, z: 0)
+                        vehicle.eulerAngles = trafficNode.directionRight ? SCNVector3Zero : SCNVector3(x: 0, y: toRadians(angle: 180), z: 0)
+                        trafficNode.addChildNode(vehicle)
+                    }
+                }
             }
         }
     }

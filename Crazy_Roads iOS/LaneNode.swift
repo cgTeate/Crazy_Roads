a2219886dd134b53eea545fa7a5ffe7d26e9fc1c
Calvin Teater <laughingglint3@gmail.com>
2023-04-19 10:38:27 -0500

    created methods to add vegetation elements to the each grass lane, vehicle lanes will be added to roads next

--- Crazy_Roads iOS/LaneNode.swift
+++ Crazy_Roads iOS/LaneNode.swift
@@ -15,6 +15,7 @@ class LaneNode: SCNNode {
     
     let type: LaneType
     
+    //creates lane depending on the type (grass or road)
     init(type: LaneType, width: CGFloat) {
         self.type = type
         super.init()
@@ -33,6 +34,7 @@ class LaneNode: SCNNode {
         }
     }
     
+    //creates a visual node from the type
     func createLane(width: CGFloat, height: CGFloat, image: UIImage) {
         let laneGeometry = SCNBox(width: width, height: height, length: 1, chamferRadius: 0)
         laneGeometry.firstMaterial?.diffuse.contents = image
@@ -42,6 +44,31 @@ class LaneNode: SCNNode {
         let laneNode = SCNNode(geometry: laneGeometry)
         
         addChildNode(laneNode)
+        addElements(width, laneNode)
+    }
+    
+    //used to add appropriate models for the each lane
+    func addElements(_ width: CGFloat, _ laneNode: SCNNode) {
+        var carGap = 0
+        
+        for index in 0..<Int(width) {
+            if type == .grass {
+                if randomBool(odds: 7) {
+                    let vegetation = getVegetation()
+                    vegetation.position = SCNVector3(x: 10 - Float(index), y: 0, z: 0)
+                    laneNode.addChildNode(vegetation)
+                }
+            } else if type == .road {
+                
+            }
+        }
+    }
+    
+    //creates a copy node of the preset model defined in Utils (treeScene, hedgeScene)
+    //reduces memory usage
+    func getVegetation() -> SCNNode {
+        let vegetation = randomBool(odds: 2) ? Models.tree.clone() : Models.hedge.clone()
+        return vegetation
     }
     
     required init?(coder aDecoder: NSCoder) {
